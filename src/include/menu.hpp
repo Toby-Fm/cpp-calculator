a121d9b03dac67f92e4ce41e597cdb03421f32c4
Toby-Fm <mail@toby-wichmann.de>
2023-10-24 23:51:05 +0200

    built switch for the menu

--- src/include/menu.hpp
+++ src/include/menu.hpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "head.hpp"
 #include "addition.hpp"
 #include "subtraction.hpp"
@@ -5,34 +7,42 @@
 #include "division.hpp"
 
 void menu() {
-    std::cout << "\nWähle deinen Modus:\n\n(1): Addieren\n(2): Subtrahieren\n(3): Multiplizieren\n(4): Dividieren\n(b): Bennden" << std::endl;
+    //Verfügbare modis anzeigen lassen
+    std::cout << "\nWähle deinen Modus:\n\n(1): Addieren\n(2): Subtrahieren\n(3): Multiplizieren\n(4): Dividieren\n(b): Beenden\n\nInput: ";
 
+    //"input" Variable erstellen
     char input = ' ';
     std::cin >> input;
 
     switch (input) {
-        case '1': {
+        case '1': { //Case 1 Addiert zahlen
             addition();
+            menu();
             break;
         }
-        case '2': {
+        case '2': { //Case 2 Subtrahiert zahlen
             subtraction();
+            menu();
             break;
         }
-        case '3': {
+        case '3': { //Case 3 Multipliziert zahlen
             multiply();
+            menu();
             break;
         }
-        case '4': {
+        case '4': { //Case 4 Dividiert zahlen
             division();
+            menu();
             break;
         }
-        case 'b': {
+        case 'b': { //Case "b" Beendet das Programm
             return;
             break;
         }
         default: {
-            std::cout << "try again" << std::endl;
+            std::cout << "\n\nEs gibt nur die Funktionen 1, 2, 3, 4 & b (Beenden). Versuche es erneut." << std::endl;
+            menu();
+            return;
             break;
         } 
     }

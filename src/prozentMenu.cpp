91419d318328e3666e29c9ae304d7b8bc2c6a08a
Toby-Fm <mail@toby-wichmann.de>
2023-10-30 04:41:08 +0100

    structure

---
+++ src/prozentMenu.cpp
@@ -0,0 +1,37 @@
+#include "include/prozent.hpp"
+
+void prozentMenu() {
+    std::cout << "\n+----------------------------------------------------------------------+\n" << std::endl;
+    std::cout << "Für welchen zweck?\n\n(1): Prozentsatz eines Wertes (x% von x€)\n(2): Prozentsatzveränderung\n"/*(3): Prozentuale Verteilung\n(4): Prozentuale Steigerung oder Senkung\n\n(i): Info/Beschreibung ansehen\n*/"(b): Beenden\n\nInput: ";
+
+    char prozentInput = ' ';
+    std::cin >> prozentInput;
+
+    switch (prozentInput) {
+        case '1': {
+            prozentsatzEinesWertes();
+            prozentMenu();
+            break;
+        }
+        case '2': {
+            przVeränderung();
+            prozentMenu();
+            break;
+        }
+        case '3': {
+            break;
+        }
+        case '4': {
+            break;
+        }
+        case 'i': {
+            //Cooming soon
+            //TODO: Beschreibungen der einzelnen M-Methoden hinschreiben
+            break;
+        }
+        case 'b': {
+            return;
+            break;
+        }
+    }
+}

20dc49da182b886f9f835e1369f326d80f2e8987
Toby-Fm <mail@toby-wichmann.de>
2023-11-07 12:35:38 +0100

    edit webrechnung

--- src/include/webRechnung.hpp
+++ src/include/webRechnung.hpp
@@ -1,7 +1,20 @@
 #pragma once
 
-#include <iostream>
+#include "head.hpp"
+#include <string>
 
-void elementInput() {
-    std::cout << "Hello" << std::endl;
+using namespace std; //Only for 'String' Variable
+
+string elementInput() {
+    string element1;
+    std::cout << "von?" << std::endl;
+    std::cin >> element1;
+
+    string element2;
+    std::cout << "zu?" << std::endl;
+    std::cin >> element2;
+
+    if (element1 = 'px' | 'PX' && element2 = 'em' | 'EM') {
+        std::cout << "hello" << std::endl;
+    }
 }
